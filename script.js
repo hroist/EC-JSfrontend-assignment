a698704c68de476dbda0e18ade19b395c829627a
hroist <111881583+hroist@users.noreply.github.com>
2022-10-11 13:23:09 +0200

    navbar and search box added

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let searchBtn = document.querySelector('.searchBtn')
+let closeBtn = document.querySelector('.closeBtn')
+let searchBox = document.querySelector('.searchBox')
+let navbar = document.querySelector('.navbar')
+let navbarToggle = document.querySelector('.navbarToggle')
+let topMenu = document.querySelector('.top-menu')
+
+searchBtn.onclick = function () {
+    searchBox.classList.add('active');
+    closeBtn.classList.add('active');
+    searchBtn.classList.add('active');
+}
+
+closeBtn.onclick = function () {
+    searchBox.classList.remove('active');
+    closeBtn.classList.remove('active');
+    searchBtn.classList.remove('active');
+}
+
+navbarToggle.onclick = function () {
+    topMenu.classList.toggle('open')
+}
+
 const onSubmit = event => {
     event.preventDefault()
     validate(event)
